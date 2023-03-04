2afc951ef838a902e91d593e0cf91c022f111e5e
vilmars baumanis <vilmarsb@VBComputer.localdomain>
2023-03-04 17:12:49 +0200

    Added simple GUI using PyQt6

---
+++ main.py
@@ -0,0 +1,31 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLineEdit, QTextEdit, QPushButton
+
+
+class ChatbotWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setMinimumSize(700, 500)
+
+        # Chat area widget
+        self.chat_area = QTextEdit(self)
+        self.chat_area.setGeometry(10,10,480,320)
+        self.chat_area.setReadOnly(True)
+        # Input field widget
+        self.input_field = QLineEdit(self)
+        self.input_field.setGeometry(10,350,480,50)
+        # Add the button
+        self.button = QPushButton("Send", self)
+        self.button.setGeometry(500,350,50,50)
+
+        self.show()
+
+
+class Chatbot:
+    pass
+
+
+app = QApplication(sys.argv)
+main_window = ChatbotWindow()
+sys.exit(app.exec())
